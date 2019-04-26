1fdc5cb8830f09629fbdf7e8d60091f189ccd324
U+ <davidyujia@gmail.com>
2019-04-26 10:55:21 +0800

    init

---
+++ QueueProcess/QueueProcess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace QueueProcess
+{
+    public class QueueProcess<T>
+    {
+        private readonly Action<T> _action;
+
+        public QueueProcess(Action<T> action)
+        {
+            _action = action;
+            this._queue = new ConcurrentQueue<T>();
+        }
+
+        private readonly ConcurrentQueue<T> _queue;
+
+        private Task _task;
+
+        private readonly object _lock = new object();
+
+        public async Task AddAsync(T item) => await Task.Factory.StartNew(() => Add(item));
+
+        public void Add(T item)
+        {
+            this._queue.Enqueue(item);
+
+            if (_task == null || this._task.Status != TaskStatus.Running)
+            {
+                lock (_lock)
+                {
+                    if (_task == null || this._task.Status != TaskStatus.Running)
+                    {
+                        _task = new Task(this.Run);
+                        _task.Start();
+                    }
+                }
+            }
+        }
+
+        private void Run()
+        {
+            while (this._queue.TryDequeue(out T item))
+            {
+                _action(item);
+            }
+        }
+    }
+}
