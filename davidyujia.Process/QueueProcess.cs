e9b82ada787d2724acb41ccc62563c1fa52046c1
U+ <davidyujia@gmail.com>
2019-06-21 13:00:48 +0800

    change namespace

--- davidyujia.Process/QueueProcess.cs
+++ davidyujia.Process/QueueProcess.cs
@@ -2,7 +2,7 @@
 using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
-namespace QueueProcess
+namespace davidyujia.Process.QueueProcess
 {
     public sealed class QueueProcess<T>
     {
