e97002d4dce1d6000ab61edfe3dd8ebab2c63585
davidyujia <davidyujia@gmail.com>
2019-06-27 18:01:17 +0800

    add base58

---
+++ davidyujia.Configuration.Crypter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace davidyujia.Configuration.Crypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        }
+    }
+}
