aabe86573de6797c8e2cd9982f20b23bf29a5b12
davidyujia <davidyujia@gmail.com>
2019-08-08 14:15:41 +0800

    add embedded resource method

---
+++ davidyujia.Unknow/EmbeddedResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace davidyujia.Unknow
+{
+    public static class EmbeddedResource
+    {
+        public static Stream Load(string nameSpace, string fileName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream($"{nameSpace}.{fileName}");
+            return stream;
+        }
+    }
+}
